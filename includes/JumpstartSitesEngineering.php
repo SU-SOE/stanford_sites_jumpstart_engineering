53c57e9b35e5a42aaa969ecd0d0e75ce8dc6b59d
Shea McKinney <sherakama@gmail.com>
2015-09-21 17:39:12 -0700

    SOE-250: Added importer and mapper configuration to install profile

--- includes/JumpstartSitesEngineering.php
+++ includes/JumpstartSitesEngineering.php
@@ -48,6 +48,14 @@ class JumpstartSitesEngineering extends JumpstartSitesAcademic {
       'run' => INSTALL_TASK_RUN_IF_NOT_COMPLETED,
     );
 
+    $tasks['jse_configure_capx'] = array(
+      'display_name' => st('Create CAPx default configuration.'),
+      'display' => FALSE,
+      'type' => 'normal',
+      'function' => 'capx_default_configuration',
+      'run' => INSTALL_TASK_RUN_IF_NOT_COMPLETED,
+    );
+
     $this->prepare_tasks($tasks, get_class());
     return array_merge($parent_tasks, $tasks);
   }
@@ -159,4 +167,284 @@ class JumpstartSitesEngineering extends JumpstartSitesAcademic {
     $time_diff = time() - $time;
     drush_log('JSE - Finished creating Private Pages menu items: ' . $time_diff . ' seconds' , 'ok');
   }
+
+
+  /**
+   * Installs default settings and creates an importer and a mapper.
+   *
+   * @param [type] &$install_state
+   *   Description.
+   */
+  public function capx_default_configuration(&$install_state) {
+    drush_log('JSE - Configuring CAPx Defaults' , 'status');
+
+    // Loop through the mapper settings and save to the DB.
+    $mappers = $this->get_capx_mappers();
+    foreach ($mappers as $machine_name => $settings) {
+
+      $mapper = entity_create('capx_cfe', array());
+
+      $mapper->uid = 1;
+      $mapper->title = $settings['title'];
+      $mapper->machine_name = $machine_name;
+      $mapper->type = "mapper";
+      $mapper->settings = $settings;
+
+      // Do the save.
+      capx_mapper_save($mapper);
+    }
+
+    // Loop through the importer configurations and save them to the db.
+    $importers = $this->get_capx_importers();
+    foreach ($importers as $machine_name => $info) {
+      $settings = $info['settings'];
+      $meta = $info['meta'];
+
+      $importer = entity_create('capx_cfe', array());
+      $importer->type = "importer";
+      $importer->machine_name = $machine_name;
+      $importer->title = $info['title'];
+      $importer->uid = 1;
+      $importer->settings = $settings;
+      $importer->meta = $meta;
+
+      // Do the save.
+      capx_importer_save($importer);
+    }
+
+    drush_log('JSE - Finished Configuring CAPx' , 'status');
+  }
+
+
+  /**
+   * Returns an array of mapper information to be used to save to the db.
+   *
+   * @return array
+   *   An array of mapper information.
+   */
+  private function get_capx_mappers() {
+    $mappers = array();
+
+    // Default Mapper.
+    $mappers['default'] = array(
+      'fields' => array(
+        'field_s_person_affiliation' => array(
+          'tid' => '',
+        ),
+        'field_s_person_cohort' => array(
+          'value' => '',
+        ),
+        'field_s_person_dissertatn_title' => array(
+          'value' => '',
+        ),
+        'field_s_person_education' => array(
+          'value' => '$.education.*.label.text',
+        ),
+        'field_s_person_email' => array(
+          'email' => '$.primaryContact.email',
+        ),
+        'field_s_person_faculty_title' => array(
+          'value' => '$.longTitle[0]',
+        ),
+        'field_s_person_faculty_type' => array(
+          'tid' => '',
+        ),
+        'field_s_person_fax_display' => array(
+          'value' => '$.primaryContact.fax',
+        ),
+        'field_s_person_file' => array(
+          0 => '',
+        ),
+        'field_s_person_first_name' => array(
+          'value' => '$.names.preferred.firstName',
+        ),
+        'field_s_person_graduation_year' => array(
+          'value' => '',
+        ),
+        'field_s_person_info_links' => array(
+          'url' => '',
+          'title' => '',
+        ),
+        'field_s_person_interests' => array(
+          'tid' => '',
+        ),
+        'field_s_person_last_name' => array(
+          'value' => '$.names.preferred.lastName',
+        ),
+        'field_s_person_mail_address_dspl' => array(
+          'value' => '$.primaryContact.address',
+        ),
+        'field_s_person_mail_code' => array(
+          'value' => '',
+        ),
+        'field_s_person_middle_name' => array(
+          'value' => '',
+        ),
+        'field_s_person_office_hours' => array(
+          'value' => '',
+        ),
+        'field_s_person_office_location' => array(
+          'value' => '',
+        ),
+        'field_s_person_phone_display' => array(
+          'value' => '',
+        ),
+        'field_s_person_profile_picture' => array(
+          0 => '',
+        ),
+        'field_s_person_staff_type' => array(
+          'tid' => '',
+        ),
+        'field_s_person_student_type' => array(
+          'tid' => '',
+        ),
+        'field_s_person_study' => array(
+          'tid' => '',
+        ),
+        'field_s_person_weight' => array(
+          'value' => '',
+        ),
+        'body' => array(
+          'value' => '',
+          'summary' => '',
+        ),
+      ),
+      'properties' => array(
+        'title' => '$.displayName',
+      ),
+      'collections' => array(),
+      'entity_type' => 'node',
+      'bundle_type' => 'stanford_person',
+      'title' => t("Default"),
+    );
+
+    // JSE Default.
+    $mappers["jse_default"] = array(
+      'fields' => array(
+        'field_s_person_affiliation' => array(
+          'tid' => '',
+        ),
+        'field_s_person_cohort' => array(
+          'value' => '',
+        ),
+        'field_s_person_dissertatn_title' => array(
+          'value' => '',
+        ),
+        'field_s_person_education' => array(
+          'value' => '$.education.*.label.text',
+        ),
+        'field_s_person_email' => array(
+          'email' => '$.primaryContact.email',
+        ),
+        'field_s_person_faculty_title' => array(
+          'value' => '$.titles.*.label.text',
+        ),
+        'field_s_person_faculty_type' => array(
+          'tid' => '',
+        ),
+        'field_s_person_fax_display' => array(
+          'value' => '$.primaryContact.fax',
+        ),
+        'field_s_person_file' => array(
+          0 => '',
+        ),
+        'field_s_person_first_name' => array(
+          'value' => '$.names.preferred.firstName',
+        ),
+        'field_s_person_graduation_year' => array(
+          'value' => '',
+        ),
+        'field_s_person_info_links' => array(
+          'url' => '$.internetLinks.*.url',
+          'title' => '$.internetLinks.*.label.text',
+        ),
+        'field_s_person_interests' => array(
+          'tid' => '',
+        ),
+        'field_s_person_last_name' => array(
+          'value' => '$.names.preferred.lastName',
+        ),
+        'field_s_person_mail_address_dspl' => array(
+          'value' => '$.primaryContact.address',
+        ),
+        'field_s_person_mail_code' => array(
+          'value' => '',
+        ),
+        'field_s_person_middle_name' => array(
+          'value' => '$.names.preferred.middleName',
+        ),
+        'field_s_person_office_hours' => array(
+          'value' => '',
+        ),
+        'field_s_person_office_location' => array(
+          'value' => '',
+        ),
+        'field_s_person_phone_display' => array(
+          'value' => '$.primaryContact.phoneNumbers.*',
+        ),
+        'field_s_person_profile_picture' => array(
+          0 => '$.profilePhotos.bigger',
+        ),
+        'field_s_person_staff_type' => array(
+          'tid' => '',
+        ),
+        'field_s_person_student_type' => array(
+          'tid' => '',
+        ),
+        'field_s_person_study' => array(
+          'tid' => '',
+        ),
+        'field_s_person_weight' => array(
+          'value' => '',
+        ),
+        'body' => array(
+          'value' => '$.bio.text',
+          'summary' => '',
+        ),
+      ),
+      'properties' => array(
+        'title' => '$.displayName',
+      ),
+      'collections' => array(),
+      'entity_type' => 'node',
+      'bundle_type' => 'stanford_person',
+      'title' => t("JSE Default"),
+    );
+
+    return $mappers;
+  }
+
+
+  /**
+   * Returns an array of importer configuration information.
+   *
+   * @return array
+   *   An array of importer configuration information.
+   */
+  private function get_capx_importers() {
+    $importers = array();
+
+    // Default.
+    $importers['default'] = array();
+    $importers['default']['title'] = t("Default");
+    $importers['default']['settings'] = array(
+      'mapper' => 'jse_default',
+      'organization' => '',
+      'child_orgs' => 0,
+      'workgroup' => '',
+      'sunet_id' => 'tsui,sheamck,regula,olgary,whitmore,chu,rbaltman,katekate,tomq1,herschla,harbury,rhiju,kobilka,ingmar,danb,hblau,pblumen,lynnw',
+      'cron_option' => 'daily',
+      'cron_option_day_number' => '1',
+      'cron_option_day_week' => 'monday',
+      'cron_option_month' => '0',
+      'cron_option_hour' => '2:00',
+      'orphan_action' => 'unpublish',
+    );
+    $importers['default']['meta'] = array();
+
+    return $importers;
+  }
+
+
+
  }
