c6d9c1cb21869ae3061855bfb94a94b98ea7a895
Caryl Westerberg <cjwest@acm.org>
2015-09-25 08:56:59 -0700

    soe-364: Added beans to import for homepage layouts

--- includes/JumpstartSitesEngineering.php
+++ includes/JumpstartSitesEngineering.php
@@ -628,11 +628,12 @@ class JumpstartSitesEngineering extends JumpstartSitesAcademic {
   private function fetch_jse_content_beans($endpoint) {
 
     $uuids = array(
-      'e813c236-7400-4f43-ad18-736617ceb28e', // Jumpstart Home Page Banner Image.
+      '04cef32d-aa4b-477c-850e-e9efd331fa4c', // Jumpstart Home Page Banner - No Caption.
       '40cabca1-7d44-42bf-a012-db53fdccd350', // Jumpstart Large Custom Block.
       '7e510af6-c003-402d-91a4-7480dac1484a', // Jumpstart Small Custom Block.
       '2c570a0a-d52a-4e8b-bf36-ec01b2777932', // JSE Logo Block.
       '87527e6a-1f9e-4b39-a999-c138851b3a47', // Jumpstart Custom Footer Block.
+      'afb406ad-c08f-4c91-a179-e703a8afc6ca', // Jumpstart Home Page Full-Width Banner - No Caption
     );
 
     $importer = new SitesContentImporter();
