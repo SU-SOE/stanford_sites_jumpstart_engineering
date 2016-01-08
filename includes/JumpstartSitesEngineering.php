0249a92e7e56ecf4ea7b979a52c2d4ad44f80dc0
Caryl Westerberg <cjwest@acm.org>
2016-01-08 15:18:56 -0800

    SOE-419: Added a ->save();

--- includes/JumpstartSitesEngineering.php
+++ includes/JumpstartSitesEngineering.php
@@ -976,6 +976,8 @@ class JumpstartSitesEngineering extends JumpstartSitesAcademic {
     t('Context for people pages using CAP fields');
     t('People');
 
+    $context->save();
+
     $time_diff = time() - $time;
     drush_log('JSE - Configuring person CAP displays: ' . $time_diff . ' seconds', 'ok');
   }
